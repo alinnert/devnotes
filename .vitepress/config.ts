0e7608374c0a175e5c06a6e637260e7f8ed46d6c
Andreas Linnert <andreas@linnertmedia.de>
2023-05-25 16:36:44 +0200

    Add git notes, clean up files

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -9,27 +9,36 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
+      { text: 'Git', link: '/git/befehle' },
       { text: 'Linux', link: '/linux/datei-operationen' },
     ],
 
-    sidebar: [
-      {
-        text: 'Linux',
-        items: [
-          { text: 'Datei-Operationen', link: '/linux/datei-operationen' },
-          { text: 'Dateien übertragen', link: '/linux/datei-transfer' },
-          {
-            text: 'Dateien packen/entpacken',
-            link: '/linux/datei-packen-entpacken',
-          },
-          { text: 'Dienste', link: '/linux/system-dienste' },
-          { text: 'System-Infos', link: '/linux/system-infos' },
-          { text: 'Laptop', link: '/linux/hardware-laptop' },
-          { text: 'Netz-Laufwerke', link: '/linux/netzlaufwerke' },
-          { text: 'GUI', link: '/linux/gui' },
-        ],
-      },
-    ],
+    sidebar: {
+      '/git/': [
+        {
+          text: 'Git',
+          items: [{ text: 'Wichtige Befehle', link: '/git/befehle' }],
+        },
+      ],
+      '/linux/': [
+        {
+          text: 'Linux',
+          items: [
+            { text: 'Datei-Operationen', link: '/linux/datei-operationen' },
+            { text: 'Dateien übertragen', link: '/linux/datei-transfer' },
+            {
+              text: 'Dateien packen/entpacken',
+              link: '/linux/datei-packen-entpacken',
+            },
+            { text: 'Dienste', link: '/linux/system-dienste' },
+            { text: 'System-Infos', link: '/linux/system-infos' },
+            { text: 'Laptop', link: '/linux/hardware-laptop' },
+            { text: 'Netz-Laufwerke', link: '/linux/netzlaufwerke' },
+            { text: 'GUI', link: '/linux/gui' },
+          ],
+        },
+      ],
+    },
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/alinnert/devnotes' },
